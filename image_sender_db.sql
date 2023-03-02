55d6cf14957359ab10beca1c7e5de3c52399d7fe
Mikhail Grebenkin <grebenkinmd@gmail.com>
2023-03-02 16:07:39 +0300

    Added .sql file to create a DB

---
+++ image_sender_db.sql
@@ -0,0 +1,14 @@
+--
+-- File generated with SQLiteStudio v3.3.3 on Чт марта 2 16:06:39 2023
+--
+-- Text encoding used: UTF-8
+--
+PRAGMA foreign_keys = off;
+BEGIN TRANSACTION;
+
+-- Table: IMAGES
+DROP TABLE IF EXISTS IMAGES;
+CREATE TABLE IMAGES (IMG_BLOBS BLOB);
+
+COMMIT TRANSACTION;
+PRAGMA foreign_keys = on;
