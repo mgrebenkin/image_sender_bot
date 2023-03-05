c0e8a6ae6ca1e9321b9f0fd2a201a8854de24ee9
Mikhail Grebenkin <grebenkinmd@gmail.com>
2023-03-05 14:37:16 +0300

    Asynchronous scheduling for messaging - Just basic implementation for now

--- server.py
+++ server.py
@@ -5,6 +5,9 @@ from aiogram import Bot, Dispatcher, executor, types
 from images_db import ImagesDB
 import exceptions
 import io
+import aioschedule
+from asyncio import sleep as aiosleep
+
 
 logger.add('log/log.log', retention=1)
 
@@ -35,13 +38,14 @@ async def store_image(message: types.Message):
     await message.photo[-1].download(destination_file=img_blob)
     try:
         img_db.store_image(img_blob=img_blob)
+        await message.answer("Изображение сохранено.")
     except exceptions.CantStoreImage:
-        await message.answer("Не удалось сохранить изображение")
+        await message.answer("Не удалось сохранить изображение.")
 
 
 @dp.message_handler(commands=['send'])
 @auth
-async def send_random_image(message: types.Message):
+async def answer_with_random_image(message: types.Message):
     try:
         fetched_image = types.InputFile(img_db.fetch_random_image())
     except exceptions.CantFetchImage as e:
@@ -49,9 +53,27 @@ async def send_random_image(message: types.Message):
     else:
         await message.answer_photo(photo=fetched_image)
 
+async def send_random_image(user_id: int ):
+    try:
+        fetched_image = types.InputFile(img_db.fetch_random_image())
+    except exceptions.CantFetchImage as e:
+        await bot.send_message(chat_id=user_id, text="Не удалось получить изображение.")
+    else:
+        await bot.send_photo(chat_id=user_id, photo=fetched_image)
+
+async def test(user_id: int):
+    await bot.send_message(chat_id=user_id, text="test")
+
+async def startup_routine(_):
+    aioschedule.every().day.at('15:00').do(send_random_image, user_id=None)
+    while True:
+        await aioschedule.run_pending()
+        await aiosleep(0.5)
+        
+
 if __name__ == '__main__':
     try:
-        executor.start_polling(dispatcher=dp, skip_updates=True, on_startup=None)
+        executor.start_polling(dispatcher=dp, skip_updates=True, on_startup=startup_routine)
     except Exception as e:
         logger.exception("Не удалось авторизовать бота: ", e)
         
