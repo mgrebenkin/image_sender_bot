c8f92eeec90863829dd6ba386dbcbc50a634ce75
Mikhail Grebenkin <grebenkinmd@gmail.com>
2023-03-10 17:46:09 +0300

    Fixed async scheduling. Added other improvements - Fixed async scheduling - Added constants for task loop period and time of daily sending - Ensured exit in case of bad critical env vars or bot inititialization failure - Added docstrings - Added more logging - Edited .gitignore

--- constants.py
+++ constants.py
@@ -7,15 +7,16 @@ import json
 load_dotenv(find_dotenv())
 
 try:
-    IMAGE_SENDER_BOT_API_TOKEN = getenv('IMAGE_SENDER_BOT_API_TOKEN')
-    if IMAGE_SENDER_BOT_API_TOKEN is None:
+    IMAGE_SENDER_BOT_API_TOKEN = getenv('IMAGE_SENDER_BOT_API_TOKEN', '')
+    if (IMAGE_SENDER_BOT_API_TOKEN is None) or (IMAGE_SENDER_BOT_API_TOKEN == ''):
         raise Exception("Не удалось получить токен бота.")
     
-    DB_PATH = getenv('DB_PATH')
-    if DB_PATH is None:
+    DB_PATH = getenv('DB_PATH', '')
+    if (DB_PATH is None) or (DB_PATH == ''):
         raise Exception("Не указано расположение базы данных.")
 except Exception as e:
-    logger.warning("Ошибка переменной окружения: ", e)
+    logger.exception("Ошибка переменной окружения: ", e)
+    exit()
 
 AUTH_ENABLED = getenv('ENABLE_AUTH', 'False').lower() in ['true', 't', '1', 'yes', 'y']
 if AUTH_ENABLED: 
@@ -27,9 +28,16 @@ try:
     USERNAME_WHITELIST = set(json.load(
         open('username_whitelist.json', 'r', encoding='utf-8')
     ))
-except OSError:
+    """List of authorized usernames"""
+except OSError as e:
     logger.exception("Ошибка чтения файла:", e)
     USERNAME_WHITELIST=set()
-    
+
 if (AUTH_ENABLED) and (len(USERNAME_WHITELIST) == 0):
-    logger.warning("Список авторизаванных пользователей пуст!")
+    logger.warning("Список авторизованных пользователей пуст!")
+
+TASK_LOOP_PERIOD = 0.5
+"""Time of sleep between aioschedule checks"""
+
+DAILY_SENDING_TIME = '15:00'
+"""Time of day to do daily sending"""
--- server.py
+++ server.py
@@ -1,12 +1,13 @@
 from loguru import logger
 
-from constants import IMAGE_SENDER_BOT_API_TOKEN, AUTH_ENABLED, DB_PATH, USERNAME_WHITELIST
+from constants import IMAGE_SENDER_BOT_API_TOKEN, AUTH_ENABLED, \
+    DB_PATH, USERNAME_WHITELIST, TASK_LOOP_PERIOD, DAILY_SENDING_TIME
 from aiogram import Bot, Dispatcher, executor, types
 from images_db import ImagesDB
 import exceptions
 import io
 import aioschedule
-from asyncio import sleep as aiosleep
+import asyncio
 
 
 logger.add('log/log.log', retention=1)
@@ -16,15 +17,23 @@ try:
     dp = Dispatcher(bot=bot)
 except Exception as e:
     logger.exception("Ошибка инициализации бота:", e)
+    exit()
 else:
     logger.info("Бот инициализирован.")
 
 img_db = ImagesDB(DB_PATH)
 
-def auth(func):
+async def do_daily_sending():
+    """Starts a loop that checks for aioschedule jobs to do"""
+    while True:
+        await aioschedule.run_pending()
+        await asyncio.sleep(TASK_LOOP_PERIOD)
 
+def auth(func):
+    """Decorator to authorize user"""
     async def wrapper(message: types.Message):
         if not (message.from_user.username in USERNAME_WHITELIST) and (AUTH_ENABLED):
+            logger.info(f"Отказано в доступе пользователю {message.from_user.username}.")
             return await message.reply("Нет доступа")
         return await func(message)
     
@@ -34,41 +43,51 @@ def auth(func):
 @dp.message_handler(content_types=types.ContentTypes.PHOTO)
 @auth
 async def store_image(message: types.Message):
+    """Saves image sent by user to database"""
     img_blob = io.BytesIO()
     await message.photo[-1].download(destination_file=img_blob)
     try:
         img_db.store_image(img_blob=img_blob)
         await message.answer("Изображение сохранено.")
-    except exceptions.CantStoreImage:
+        logger.info(f"Изображение от пользователя {message.from_user.username} сохранено.")
+    except exceptions.CantStoreImage as e:
+        logger.exception(f"Не удалось сохранить изображение от пользователя {message.from_user.username}", e)
         await message.answer("Не удалось сохранить изображение.")
 
 
 @dp.message_handler(commands=['send'])
 @auth
 async def answer_with_random_image(message: types.Message):
+    """Answers to user with random image from database"""
     try:
         fetched_image = types.InputFile(img_db.fetch_random_image())
     except exceptions.CantFetchImage as e:
+        logger.exception(f"Не удалось ответить пользователю {message.from_user.username}:", e)
         await message.answer(text="Не удалось получить изображение.")
     else:
         await message.answer_photo(photo=fetched_image)
+        logger.info(f"Бот ответил пользователю {message.from_user.username}.")
 
 async def send_random_image(user_id: int ):
+    """Sends random image to user with user_id"""
     try:
         fetched_image = types.InputFile(img_db.fetch_random_image())
     except exceptions.CantFetchImage as e:
+        logger.exception(f"Не удалось отправить изображение пользователю с id {user_id}", e)
         await bot.send_message(chat_id=user_id, text="Не удалось получить изображение.")
     else:
         await bot.send_photo(chat_id=user_id, photo=fetched_image)
+        logger.info(f"Отправлено изображение пользователю с id {user_id}")
 
 async def test(user_id: int):
+    """Sends "test" to user with user_id"""
     await bot.send_message(chat_id=user_id, text="test")
 
 async def startup_routine(_):
-    aioschedule.every().day.at('15:00').do(send_random_image, user_id=None)
-    while True:
-        await aioschedule.run_pending()
-        await aiosleep(0.5)
+    """Schedules daily sending jobs and starts checking loop"""
+    aioschedule.every().day.at(DAILY_SENDING_TIME).do(send_random_image, user_id=None)
+    logger.info(f"Запланирована отправка изображения пользователю с id {None}")
+    asyncio.create_task(do_daily_sending())
         
 
 if __name__ == '__main__':
